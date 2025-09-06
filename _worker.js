fa7f658b84fad02b511f7965a24564eddb1a20b8
Gino <2063598+gino2010@users.noreply.github.com>
2025-09-06 11:41:41 +0800

    update worker.js

--- _worker.js
+++ _worker.js
@@ -10,6 +10,26 @@
 
 export default {
   async fetch(request, env, ctx) {
-    return new Response(request.method + ' My World!');
+
+    const upgradeHeader = request.headers.get('Upgrade');
+    const url = new URL(request.url);
+            
+    if (!upgradeHeader || upgradeHeader !== 'websocket') {
+                
+                const 路径 = url.pathname.toLowerCase();
+                if (路径 == '/') {
+                    if (env.URL302) return Response.redirect(env.URL302, 302);
+                    // else if (env.URL) return await 代理URL(env.URL, url);
+                    else return new Response('Hello World!');
+                } else if (路径 != '/') {
+                    return new Response('Hello World! Path: ' + 路径);
+                } else {
+                    if (env.URL302) return Response.redirect(env.URL302, 302);
+                    else return new Response('should not be here');
+                }
+            }
+
+     return new Response(request.method + ' My World!');
+
   }
 };
